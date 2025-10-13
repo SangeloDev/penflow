f61a88264e448a9a80c51bcda55a158aa8f59d05
Sangelo <contact@sangelo.space>
2025-10-13 23:31:37 +0200

    chore: cleanup

--- src/ambient.d.ts
+++ src/ambient.d.ts
@@ -4,6 +4,11 @@ declare module "markdown-it-task-checkbox" {
   export default plugin;
 }
 
+declare module "markdown-it-emoji" {
+  import MarkdownIt from "markdown-it";
+  export const full: MarkdownIt.PluginSimple;
+}
+
 declare module "virtual:commit-hash" {
   const commitHash: string;
   export default commitHash;
